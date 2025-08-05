00110108c65f760b54a7f4a3688adf7f74a468da
Arthur Dias Moresco <morescoarthur@gmail.com>
2025-08-05 14:26:12 -0300

    db_SQLite

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__) # Padrão, sempre vai ser assim
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+#Modelagem
+#Produto(id, name, price, description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # Integer é o tipo de dado INTEIRO
+    name = db.Column(db.String(120), nullable=False) #Quer obrigrar que todo produto tenha nome. String vc limita o tamanho do texto
+    price = db.Column(db.Float, nullable=False) #É obrigatorio todo produto ter o preço. Float é usado para valores decimais
+    description = db.Column(db.Text, nullable=True) #Descrição é opcional. Text é ILIMITADO, difetente da String aonde vc tem limitações
+
 
 #Definir uma rota raíz (página inicial) e a função que será executada ao requisitar
 @app.route('/')
